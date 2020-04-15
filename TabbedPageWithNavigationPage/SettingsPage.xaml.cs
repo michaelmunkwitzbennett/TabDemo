bf11c7dc615f9f72033a5e11b836e501c56560c7
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2020-04-15 13:11:08 -0500

    Initial Commit

---
+++ TabbedPageWithNavigationPage/SettingsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void calculate_clicked(Object sender, EventArgs args)
+		{
+			int age = Int32.Parse(yourAge.Text);
+			if (age < 16)
+			{
+				result.Text = "You are too young to drive";
+			} else if (age == 16)
+			{
+				result.Text = "You just learned how to drive this year!";
+			} else if (age == 17)
+			{
+				result.Text = "You have been driving for 1 year";
+			} 
+			else
+			{
+				int yearsDriveable = age - 16;
+				result.Text = "You have been driving for " + yearsDriveable + " years";
+			}
+		}
+	}
+}
+
---
+++ TabbedPageWithNavigationPage/UpcomingAppointmentsPageCS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public class UpcomingAppointmentsPageCS : ContentPage
+	{
+		public UpcomingAppointmentsPageCS ()
+		{
+			
+		}
+
+		async void OnBackButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PopAsync ();
+		}
+	}
+}
